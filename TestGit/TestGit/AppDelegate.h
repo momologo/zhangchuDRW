7ed6dd66aa5bd177efd4405e519094f1d1de193c
zs <zs@163.com>
2016-03-31 11:43:05 +0800

    sss

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by qianfeng on 16/3/31.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
